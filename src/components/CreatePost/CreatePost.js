a7030e0034c5539103e6a265161f6f21ebaf6ee0
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-01 22:38:54 +0900

    create post page

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import "./CreatePost.css"
 
 const CreatePost = () => {
   return (
-    <div>CreatePost</div>
+    <div className='createPostPage'>
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入"></input>
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          {/* textareaは大きめのinputのようなもの */}
+          <textarea placeholder="投稿内容を記入"/>
+        </div>
+        <button className='postButton'>投稿する</button>
+      </div>
+    </div>
   )
 }
 
