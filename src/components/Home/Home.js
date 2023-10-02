ce4cde90193c29d18792e25734133b38ef050c34
Takuya-ops <hora2112.ipu@gmail.com>
2023-10-02 20:17:18 +0900

    home component add

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      <div className='postContents'>
+        <div className='postHeader'>
+          <h1>タイトル</h1>
+        </div>
+      </div>
+      <div className="postTextContainer">
+        今はReactを学習中です。これからフリーランスのReactエンジニアとして働いていきたいです。
+      </div>
+      <div className="nameAndDeleteButton">
+        <h3>@tacode</h3>
+        <button>削除</button>
+      </div>
+    </div>
   )
 }
 
